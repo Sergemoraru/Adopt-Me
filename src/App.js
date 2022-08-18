da534390f234f4ed81d62004b0cd6921d2fea87a
Sergemoraru <90487457+Sergemoraru@users.noreply.github.com>
2022-08-18 17:44:52 -0400

    update-1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+      ;
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
